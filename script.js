14eb94111ee21ba3175c7bf5681438bbc96c5779
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2022-06-23 14:46:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+     let x,y,z;
+     x = parseInt(prompt('input first number'))
+     y = prompt('operator')
+     z = parseInt(prompt('input second number'))
+     */
+
+//     document.write('jdjsfjd\ndjskdj skjdjsdj ' + "\n" + 'bchsbd\n\nkhsbkdskdsk');
+//     document.write('\n')
+//     document.write('djbksjdbkjsjvnfjdvndljljvnlbkvsj')
+    document.write("<strong>First Name</strong>: Wisdom" + "<br>" + "<br>" + "<strong>Last Name</strong>: Egba"+ "<br>" + "<br>" + "<strong>Height</strong>: 6 5'"+ "<br>" + "<br>" + "<strong>Nationality</strong>: Nigeria" );
+ 
